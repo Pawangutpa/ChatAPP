e764411ee83a5e4038c1130f1494e991ae18b6f1
dsjchd <pawan246g@gmail.com>
2025-06-03 20:39:10 +0530

    dot for online

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -13,7 +13,7 @@ const Conversation = ({conversation,lastIdx,emoji}) => {
         <div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer ${isSelected ?"bg-sky-500":""}`}
 		onClick={()=>setSelectedConversation(conversation)}
 		>
- 				<div className={`avatar ${isOnline? "online":""}`}>
+ 				<div className={`avatar ${isOnline? "avatar-online":""}`}>
  					<div className="w-12 rounded-full">
  						<img
  							src={conversation.profilePic}
